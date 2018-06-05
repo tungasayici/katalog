28a0eb2401883bbd11ab31fdd6886c5d8596ee5c
Yakup AD <yakupad@yandex.com>
2018-06-05 16:11:53 +0300

    home page service integration

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/controller');
+var controllerRouter = require('./routes/controller');
 
 var app = express();
 
