5f409aaabc1a6b86bac9a3ae2038c0638b0c5c2f
Yakup AD <yakupad@yandex.com>
2018-06-06 14:57:34 +0300

    product add page service connected

--- routes/controller.js
+++ routes/controller.js
@@ -10,7 +10,7 @@ router.get('/getToken', function (req, res, next) {
   });
 });
 
-/* GET login page. */
+/* Login authentication */
 router.post('/auth', function (req, res, next) {
   var headers = {
     'Content-Type': 'application/json'
@@ -20,14 +20,12 @@ router.post('/auth', function (req, res, next) {
     method: 'POST',
     headers: headers,
     json: true,
-    body: {
-      'email': req.body.email,
-      'password': req.body.password
-    }
+    body: req.body
   }
 
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      console.log(body);
       global.authProfile = body;
       global.AUTHEMAIL = req.body.email;
       global.AUTHPASSWORD = req.body.password;
@@ -36,6 +34,32 @@ router.post('/auth', function (req, res, next) {
       res.send(error);
     }
   })
+});
+
+/* Login authentication */
+router.post('/addStartup', function (req, res, next) {
+  helper.tokenControl(AUTHEMAIL, AUTHPASSWORD, function (token) {
+    console.log(token)
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    }
+    var options = {
+      url: 'https://katalog-backend.herokuapp.com/startup/create',
+      method: 'POST',
+      headers: headers,
+      json: true,
+      body: req.body
+    }
+  console.log(options);
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }else{
+        res.send(error);
+      }
+    })
+  });
 });
 
 module.exports = router;
